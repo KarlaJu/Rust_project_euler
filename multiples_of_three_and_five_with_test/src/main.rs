3837cfd85efa07f8f51035ecb94ce698cc3bce3d
Karla <karla@makingdevs.com>
2018-09-05 18:33:57 -0500

    Give a name more explicit

--- multiples_of_three_and_five_with_test/src/main.rs
+++ multiples_of_three_and_five_with_test/src/main.rs
@@ -1,19 +1,20 @@
-pub fn multiples() -> i16{
-  let mut sum = 0;
-  for i in 0..10{
-    if i % 3 == 0  || i % 5 == 0{
-      sum = sum + i;
+pub fn multiples() -> i16 {
+  let mut sum_of_multiples = 0;
+  for number in 0..10{
+    if number % 3 == 0  || number % 5 == 0{
+      sum_of_multiples = sum_of_multiples + i;
     }
   }
   return sum
 }
+
 
 #[cfg(test)]
 mod tests {
     use super::multiples;
 
     #[test]
-    fn testing_multiples_of_three() {
+    fn testing_sum_of_multiples() {
       let result = multiples();
       assert_eq!(23, result);
     }
