0ffb6e628f340e80a3d02df88403261f08411879
Karla <karla@makingdevs.com>
2018-09-05 19:19:21 -0500

    Change the name of some variables

--- multiples_of_three_and_five_with_test/src/main.rs
+++ multiples_of_three_and_five_with_test/src/main.rs
@@ -1,21 +1,25 @@
-pub fn multiples() -> i16 {
-  let mut sum_of_multiples = 0;
-  for number in 0..10{
-    if number % 3 == 0  || number % 5 == 0{
-      sum_of_multiples = sum_of_multiples + i;
-    }
-  }
-  return sum
+pub fn get_sum_of_multiples() -> i16 {
+  let sum_of_multiples = sum_multiples_of_three_and_five();
+  return sum_of_multiples
 }
 
+fn sum_multiples_of_three_and_five() -> i16 {
+  let mut sum_of_multiples = 0;
+  for number in 0..10{
+    if number % 3 == 0  || number % 5 == 0 {
+      sum_of_multiples = sum_of_multiples + number;
+    }
+  }
+  return sum_of_multiples
+}
 
 #[cfg(test)]
 mod tests {
-    use super::multiples;
+    use super::get_sum_of_multiples;
 
     #[test]
     fn testing_sum_of_multiples() {
-      let result = multiples();
+      let result = get_sum_of_multiples();
       assert_eq!(23, result);
     }
 
